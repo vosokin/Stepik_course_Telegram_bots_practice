3998f3c2fabd5aaacb954ce7b8eb875e974d9017
Viktor Osokin <56092036+vosokin@users.noreply.github.com>
2022-11-23 13:48:38 +0500

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = '5972411700:AAHV73ivGETkQ57NTOdki0V3G_OHDSEuqCE' #bot token
-greeting = 'Вы находитесь в\nТелеграм-боте Фонда "Транспортные инновации Москвы"'
-buttom_name = ['Подать заявку', 'Задать вопрос']
